f6d5cfa7ff16bf5cb4ad0e6d5a9de78170bc8614
Dave Laffan <dave.laffan@outlook.com>
2020-11-29 13:57:31 +0000

    Add logic to handle posted forms

--- short_url/forms.py
+++ short_url/forms.py
@@ -4,8 +4,18 @@ from .models import UrlObject
 
 
 class UrlObjectForm(forms.ModelForm):
-    """Form for adding a url to be shortened"""
+    """Form for providing a url to be shortened"""
 
     class Meta:
         model = UrlObject
         fields = ['long_url']
+
+
+class PrivateUrlObjectForm(UrlObjectForm):
+    """Extend the UrlObjectForm with tag fields"""
+
+    def __init__(self, *args, **kwargs):
+        super(UrlObjectForm, self).__init__(*args, **kwargs)
+        self.fields['tag_one'] = forms.CharField()
+        self.fields['tag_two'] = forms.CharField()
+        self.fields['tag_three'] = forms.CharField()
--- short_url/views.py
+++ short_url/views.py
@@ -1,18 +1,43 @@
 from django.shortcuts import render
 
-from .forms import UrlObjectForm
+from .forms import PrivateUrlObjectForm, UrlObjectForm
+
+import random
+import string
 
 
 def index(request):
-    """Front page"""
+    """Main page"""
 
-    url_form = UrlObjectForm()
+    user_active = True if request.user.is_authenticated else False
+
+    # Initialise specific form depending if a user is logged in
+    url_form = PrivateUrlObjectForm if user_active else UrlObjectForm
 
     if 'shorten-url-form' in request.POST:
 
-        form = UrlObjectForm(request.POST)
-        print(form)
-        form.save()
+        form = PrivateUrlObjectForm(request.POST) if user_active \
+            else UrlObjectForm(request.POST)
+
+        if form.is_valid():
+
+            form = form.save(commit=False)
+
+            # create short_path value
+            chars = string.ascii_lowercase + string.digits
+            form.short_path = ''.join((random.choice(chars) for i in range(5)))
+
+            # if form contains tag values, add these to the tags ArrayField
+            tag_list = []
+            for tag in ['tag_one', 'tag_two', 'tag_three']:
+                if request.POST.get(tag):
+                    tag_list.append(request.POST[tag])
+            form.tags = tag_list
+
+            form.save()
+
+        else:
+            url_form = form
 
     context = {'url_form': url_form, }
 
